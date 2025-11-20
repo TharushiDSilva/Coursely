65b0c8144ad752028830a5afe1532a0352e5d6e3
TharushiDSilva <desilva.tharushiw@gmail.com>
2025-11-20 14:43:33 +0530

    create app.js and navigation skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { store } from './redux/store';
+import AppNavigator from './navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+}
